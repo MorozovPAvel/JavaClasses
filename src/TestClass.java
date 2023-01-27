14cbbb0a37d4f6f01bde7846ce6a0f3986bdb465
pavel <morozovpavelanatolevich@gmail.com>
2023-01-27 14:48:38 +0500

    test

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,5 +3,10 @@ package src;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(seyHEllo());
+    }
+
+    public static String seyHEllo(){
+        return "Hello World";
     }
 }
